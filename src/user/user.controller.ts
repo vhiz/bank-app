028b574f57dc89a2be6bf455df59fb625d9a9c3a
victor <victormgbeahurike82@gmail.com>
2022-09-23 17:14:50 +0100

    update

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,15 +1,17 @@
 import { Controller, Get, UseGuards } from '@nestjs/common';
 import { Req } from '@nestjs/common/decorators';
-import { AuthGuard } from '@nestjs/passport';
 import { Request } from 'express';
+import { UserDecorator } from 'src/auth/decorator';
+import { JwtGuard } from 'src/auth/guard';
+import { User } from 'src/auth/interface/user,interface';
 
 
+@UseGuards(JwtGuard)
 @Controller('user')
 export class UserController {
 
-    @UseGuards(AuthGuard('jwt'))
     @Get('me')    
-    getme(@Req() req: Request){
-        return req.user
+    getme(@UserDecorator() user: User){
+        return user
     }
 }
